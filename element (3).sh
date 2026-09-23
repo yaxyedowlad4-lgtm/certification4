1c834647e63e4119e659a9a1f0299ff203c318e1
yaxyedowlad4-lgtm <yaxyedowlad4@gmail.com>
2026-09-23 21:56:01 +0300

    Add files via upload

---
+++ element (3).sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+ELEMENT_INFO=$($PSQL "
+SELECT elements.atomic_number, elements.name, elements.symbol,
+       properties.atomic_mass,
+       properties.melting_point_celsius,
+       properties.boiling_point_celsius,
+       types.type
+FROM elements
+JOIN properties USING(atomic_number)
+JOIN types USING(type_id)
+WHERE elements.atomic_number::TEXT = '$1'
+   OR elements.symbol = '$1'
+   OR elements.name = '$1'
+")
+if [[ -z $ELEMENT_INFO ]]
+then
+  echo "I could not find that element in the database."
+  exit
+fi
+if [[ -z $ELEMENT_INFO ]]
+then
+  echo "I could not find that element in the database."
+  exit
+fi
+
+IFS='|' read ATOMIC_NUMBER NAME SYMBOL ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE <<< "$ELEMENT_INFO"
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
